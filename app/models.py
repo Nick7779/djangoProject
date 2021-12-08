e0fd5039bf3c3b14e426d13529a8d59e434f563e
Nick <jbliu@3tech.net>
2021-12-09 02:55:08 +0800

    Django Admin 生成Model中的表 更新了笔记

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django import forms
+import hashlib
+
 
 # Create your models here.
+
+class AuthUser(models.Model):
+    user_name = models.CharField(max_length=20, verbose_name="用户名", null=False, unique=True)
+    login_name = models.CharField(max_length=20, verbose_name="登录账号", null=False, unique=True)
+    e_mail = models.CharField(max_length=20, verbose_name="邮箱", null=True, unique=True)
+    password = models.CharField(max_length=20, verbose_name="密码")
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "用户"
+        verbose_name = "用户"
+        db_table = "user"
+
+    # def save(self, *args, **kwargs):
+    #     md5 = hashlib.md5()
+    #     md5.update(self.password.encode())
+    #     super(AuthUser, self).save(*args, **kwargs)
+    #
+    # def md5(self, pwd):
+    #     md5 = hashlib.md5()
+    #     md5.update(pwd.encode())
+    #     saltpassword = md5.hexdigest()
+    #     return saltpassword
+    #
+    # def sha1(key):
+    #     sha1_key = hashlib.sha1()
+    #     sha1_key.update(key.encode("utf-8"))
+    #     return sha1_key.hexdigest()
---
+++ note.md
@@ -0,0 +1,9 @@
+### Admin：
+在Django自带的admin中创建超级用户：`python manage.py createsuperuser`
+
+### Model：
+models.CharField：必须指定长度max_length=20
+
+Model生成迁徙文件：`python manage.py makemigrations`
+
+执行迁徙文件：`python manage.py migrate`
